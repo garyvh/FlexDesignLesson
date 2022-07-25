e3bd819f6751fb679572e669fb164a56a237eb14
Gary Hoang <garyhoangvinh@gmail.com>
2022-07-25 14:44:05 -0700

    js exercise 1,2,3

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Exercise 1: 
 // make toggle equal the item with the class 'toggle'
 // and menu equal the item with the class 'menu'
-const toggle = undefined;
-const menu = undefined;
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
 
 // Exercise 2:
 // makee a function called 'toggleMenu' that 
 // writes 'working' to the console
+function toggleMenu() {
+    console.log("working");
+}
 
 // Exercise 3:
 // Add an event listener to the toggle element
 // to run the toggleMenu function
+toggle.addEventListener("click", toggleMenu);
